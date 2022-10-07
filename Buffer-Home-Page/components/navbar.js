d7e6588213bdd0ed8dcac4101a855a4ad9b46b85
saikiran11461 <70562454+saikiran11461@users.noreply.github.com>
2022-10-07 19:13:32 +0530

    somechanges

--- Buffer-Home-Page/components/navbar.js
+++ Buffer-Home-Page/components/navbar.js
@@ -35,7 +35,7 @@
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-square-text" viewBox="0 0 16 16">
                   <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                   <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-                  </svg> <h6>Engagment</h6> </a>
+                  </svg> <h6>Engagement</h6> </a>
                   <p class ="Publishing">Plan,collobrate,and publish </br>thumb-stopping contant </p></li>
                  </li>                  
                   <!-- <li><hr class="dropdown-divider"></li> -->
@@ -54,7 +54,7 @@
               </li>
           
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="../About%20us/About%20Us-III/About.html">About</a>
+                <a class="nav-link active" aria-current="page" href="%20us/About%20Us-III/About.html">About</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="../AnalaticsPage/customers.html">Customers</a>
